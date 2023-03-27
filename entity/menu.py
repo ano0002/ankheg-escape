15d4f783d7a16a2460fe89ac8b89df8b025e9450
Anatole Sot <47571181+ano0002@users.noreply.github.com>
2023-03-27 17:29:30 +0200

    Added a setting menu

--- entity/menu.py
+++ entity/menu.py
@@ -30,8 +30,10 @@ class UI(Entity):
     def __init__(self,player,start=None, **kwargs):
         super().__init__(parent = camera.ui,**kwargs)
         self.start = start
-        self.play_button = UI_Button(main_ui=self,texture="./assets/ui/play_button.png", scale = Vec2(0.2,0.1), position = (0,0), on_click = self._start)
-        self.main_menu = [self.play_button]
+        self.play_button = UI_Button(main_ui=self,texture="./assets/ui/play_button.png", scale = Vec2(0.2,0.1), position = (-0.5,0), on_click = self._start)
+        self.setting_button = UI_Button(main_ui=self,texture="./assets/ui/setting_button.png", scale = Vec2(0.2,0.1), position = (-0.5,-0.2), on_click = self.open_setting)
+        self.main_menu = [self.play_button,self.setting_button]
+        self.setting_menu = []
         self.cursor = Entity(parent = camera.ui, model = 'quad', scale = (0.04,0.04), origin = (-0.5,0), texture = './assets/ui/cursor.png')
         self.player = player
     
@@ -45,3 +47,14 @@ class UI(Entity):
         for child in self.main_menu:
             child.disable()
         self.start()
+    
+    def open_setting(self):
+        for child in self.main_menu:
+            child.disable()
+        self.setting_menu.append(UI_Button(main_ui=self,texture="./assets/ui/setting_button.png", scale = Vec2(0.2,0.1), position = (-0.5,-0.2), on_click = self.close_setting))
+    
+    def close_setting(self):
+        for child in self.setting_menu:
+            child.disable()
+        for child in self.main_menu:
+            child.enable()
